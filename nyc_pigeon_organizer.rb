1378134d9fddeb2472194ed5d48b4f16b1c7c027
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-15 19:59:30 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+    data.each do |hash, info|
+      info.each do |key, value|
+        value.each do |name|
+          if !new_hash.include?(name)
+          new_hash[name] = {}
+        end
+
+        if !new_hash[name].include?(hash)
+          new_hash[name][hash] = []
+        end
+
+        if !new_hash[name][hash].include?(key)
+          new_hash[name][hash] << key.to_s
+        end
+      end
+    end
+  end
+  new_hash
 end
